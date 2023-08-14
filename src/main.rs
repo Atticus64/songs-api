d5451402610a74805dbb87bc27d24d8b37ebe788
Jonathan <jonathanelian64@gmail.com>
2023-08-14 13:59:27 -0600

    feat: Add cors

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::net::SocketAddr;
 use axum::{routing::get, Router};
 
 use crate::{controllers::hello_world, routes::songs::songs_routes};
+use tower_http::cors::{Any, CorsLayer};
+use http::Method;
 
 mod controllers;
 mod models;
@@ -12,7 +14,16 @@ mod schema;
 #[tokio::main]
 async fn main() {
     let port = dotenvy::var("PORT").unwrap_or("8080".to_string());
+
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any);
+
+
     let app = Router::new()
+        .layer(cors)
         .nest("/songs", songs_routes())
         .route("/", get(hello_world));
 
