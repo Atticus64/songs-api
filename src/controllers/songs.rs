1a1172f641d1357180d33d2c6a0d6ff81238d66e
Jonathan <jonathanelian64@gmail.com>
2023-08-14 22:53:06 -0600

    feat: add pagination to search

--- src/controllers/songs.rs
+++ src/controllers/songs.rs
@@ -1,11 +1,14 @@
 use axum::{Json, extract::Query};
 use diesel::{RunQueryDsl, QueryDsl, SelectableHelper, TextExpressionMethods, sql_function};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::models::Vers;
 use crate::{db::establish_connection, models::Song};
 use crate::schema::songs::dsl::*;
 use crate::schema::verses::dsl::*;
+
+sql_function!(fn lower(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarChar);
+sql_function!(fn unaccent(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarChar);
 
 pub async fn get_songs() -> Json<Vec<Song>> {
 
@@ -26,17 +29,47 @@ pub struct Pagination {
     per_page: Option<usize>,
 }
 
+#[derive(Deserialize, Serialize)]
+pub struct ResultSearch {
+    data: Vec<(Vers, Song)>,
+    count: i64,
+    page: usize,
+    items: usize,
+    total_pages: usize,
+    per_page: usize
+}
 
-pub async fn search_song(query: Query<Pagination>) -> Json<Vec<(Vers, Song)>> {
+pub async fn search_song(query: Query<Pagination>) -> Json<ResultSearch> {
 
     let mut connection = establish_connection();
-    sql_function!(fn lower(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarChar);
-    sql_function!(fn unaccent(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarChar);
+
+    let page = query.page.unwrap_or(1) - 1;
+
+    let offset = page * query.per_page.unwrap_or(10);
+
+    let count: i64 = songs.inner_join(verses)
+        .filter(unaccent(lower(content)).like(format!("%{}%", query.q)))
+        .count()
+        .get_result(&mut connection).unwrap();
+
+    let per_page = query.per_page.unwrap_or(10);
+
     let data = songs.inner_join(verses)
         .select((Vers::as_select(), Song::as_select()))
         .filter(unaccent(lower(content)).like(format!("%{}%", query.q)))
-        .distinct_on(content)
+        .limit(per_page.try_into().unwrap())
+        .offset(offset.try_into().unwrap())
         .load::<(Vers, Song)>(&mut connection).unwrap();
 
-    Json(data)
+    let len = data.len();
+
+    Json(ResultSearch {
+        data,
+        count,
+        items: len,
+        page: page + 1,
+        per_page: query.per_page.unwrap_or(10),
+        total_pages: (count as usize / per_page) + 1
+    })
 }
+
