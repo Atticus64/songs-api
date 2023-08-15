19df8405e34e514c3738f8c92cf11c94f5628826
Jonathan <jonathanelian64@gmail.com>
2023-08-14 19:51:24 -0600

    feat: update migrations

--- migrations/2023-08-13-224421_create_songs/down.sql
+++ migrations/2023-08-13-224421_create_songs/down.sql
@@ -1,3 +1,4 @@
 
 DROP TABLE IF EXISTS songs cascade;
+DROP TYPE IF EXISTS Category cascade;
 DROP TABLE IF EXISTS verses cascade;
--- migrations/2023-08-13-224421_create_songs/up.sql
+++ migrations/2023-08-13-224421_create_songs/up.sql
@@ -1,17 +1,17 @@
 -- Your SQL goes here
 
--- CREATE TYPE Category AS ENUM ('exaltacion', 'adoracion', 'ministracion');
+CREATE TYPE Category AS ENUM ('exaltacion', 'adoracion', 'ministracion');
 
 CREATE TABLE songs (
-	id INTEGER NOT NULL PRIMARY KEY ,
-	name TEXT NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	title TEXT NOT NULL,
 	category Category NOT NULL,
-	interpreter TEXT NOT NULL,
+	interpreter TEXT,
 	num_verses INTEGER NOT NULL
 );
 
 CREATE TABLE verses (
-	id INTEGER NOT NULL PRIMARY KEY,
+	id SERIAL NOT NULL PRIMARY KEY,
 	content TEXT NOT NULL,
 	song_id INTEGER NOT NULL,
 	foreign key (song_id) references songs(id)
