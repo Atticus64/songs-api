6836965300c0c585a2a7a6364190f9aa72793105
Jonathan <jonathanelian64@gmail.com>
2023-08-14 21:59:34 -0600

    feat: add search endpoint

--- src/controllers/songs.rs
+++ src/controllers/songs.rs
@@ -1,11 +1,14 @@
-use axum::Json;
-use diesel::RunQueryDsl;
+use axum::{Json, extract::Query};
+use diesel::{RunQueryDsl, QueryDsl, SelectableHelper, TextExpressionMethods, sql_function};
+use serde::Deserialize;
 
+use crate::models::Vers;
 use crate::{db::establish_connection, models::Song};
+use crate::schema::songs::dsl::*;
+use crate::schema::verses::dsl::*;
 
 pub async fn get_songs() -> Json<Vec<Song>> {
 
-    use crate::schema::songs::dsl::*;
 
     
     let mut conn = establish_connection();
@@ -16,3 +19,24 @@ pub async fn get_songs() -> Json<Vec<Song>> {
     }
 }
 
+#[derive(Deserialize)]
+pub struct Pagination {
+    page: Option<usize>,
+    q: String,
+    per_page: Option<usize>,
+}
+
+
+pub async fn search_song(query: Query<Pagination>) -> Json<Vec<(Vers, Song)>> {
+
+    let mut connection = establish_connection();
+    sql_function!(fn lower(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarChar);
+    sql_function!(fn unaccent(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarChar);
+    let data = songs.inner_join(verses)
+        .select((Vers::as_select(), Song::as_select()))
+        .filter(unaccent(lower(content)).like(format!("%{}%", query.q)))
+        .distinct_on(content)
+        .load::<(Vers, Song)>(&mut connection).unwrap();
+
+    Json(data)
+}
--- src/routes/songs.rs
+++ src/routes/songs.rs
@@ -1,7 +1,9 @@
 use axum::{routing::get, Router};
 
-use crate::controllers::songs::get_songs;
+use crate::controllers::songs::{get_songs, search_song};
 
 pub fn songs_routes() -> Router {
-    Router::new().route("/", get(get_songs))
+    Router::new()
+        .route("/", get(get_songs))
+        .route("/search", get(search_song))
 }
