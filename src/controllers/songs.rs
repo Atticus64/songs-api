9e1513ea7fe657bef0a8ce85b9e78ba591f75560
Jonathan <jonathanelian64@gmail.com>
2023-08-14 23:05:53 -0600

    feat: add lower query validation

--- src/controllers/songs.rs
+++ src/controllers/songs.rs
@@ -11,9 +11,7 @@ sql_function!(fn lower(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarC
 sql_function!(fn unaccent(a: diesel::sql_types::VarChar) -> diesel::sql_types::VarChar);
 
 pub async fn get_songs() -> Json<Vec<Song>> {
-
-
-    
+   
     let mut conn = establish_connection();
 
     match songs.load::<Song>(&mut conn) {
@@ -47,8 +45,10 @@ pub async fn search_song(query: Query<Pagination>) -> Json<ResultSearch> {
 
     let offset = page * query.per_page.unwrap_or(10);
 
+    let q = query.q.to_lowercase();
+
     let count: i64 = songs.inner_join(verses)
-        .filter(unaccent(lower(content)).like(format!("%{}%", query.q)))
+        .filter(unaccent(lower(content)).like(format!("%{}%", q)))
         .count()
         .get_result(&mut connection).unwrap();
 
@@ -56,9 +56,9 @@ pub async fn search_song(query: Query<Pagination>) -> Json<ResultSearch> {
 
     let data = songs.inner_join(verses)
         .select((Vers::as_select(), Song::as_select()))
-        .filter(unaccent(lower(content)).like(format!("%{}%", query.q)))
-        .limit(per_page.try_into().unwrap())
-        .offset(offset.try_into().unwrap())
+        .filter(unaccent(lower(content)).like(format!("%{}%", q)))
+        .limit(per_page as i64)
+        .offset(offset as i64)
         .load::<(Vers, Song)>(&mut connection).unwrap();
 
     let len = data.len();
@@ -68,7 +68,7 @@ pub async fn search_song(query: Query<Pagination>) -> Json<ResultSearch> {
         count,
         items: len,
         page: page + 1,
-        per_page: query.per_page.unwrap_or(10),
+        per_page, 
         total_pages: (count as usize / per_page) + 1
     })
 }
